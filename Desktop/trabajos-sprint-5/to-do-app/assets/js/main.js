d3f0deb6ef47fd05000bea8514710e083c487ed3
chinchillapsico <coraline.colous@gmail.com>
2017-06-28 20:51:16 -0400

    html done, some advances in js

--- Desktop/trabajos-sprint-5/to-do-app/assets/js/main.js
+++ Desktop/trabajos-sprint-5/to-do-app/assets/js/main.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
-	  $("#btn").click(function() { 
-        var add = $("input[name=listItem]").val();
-        $("input[name=listItem]").append("#list");
-    });
-  
-});
+
+$(document).ready(function(){
+
+  	$('#add').on('click', addTask); /*usamos el selector #add que al hacer click desencadena la función*/ 
+
+     
+	function addTask(){
+
+  		var  newToDo= $('#newtask').val(); /*creamos una variable que guarda el valor de la nueva tarea ingresada*/
+    	$('#listTask').append('<li>' + newToDo + '</li>'); /* tenemos la lista "ul" como contenedor vacío 
+                                                       con el id #listTask, con el método append creamos un "li"
+                                                        que se agrega la final del elemento, este "li" contiene  el valor
+                                                       de la variable declarada recién */
+    	$('#newtask').val("");/*luego de agregar el elemento deja vacío el campo otra vez.*/
+  	}
+});  	
